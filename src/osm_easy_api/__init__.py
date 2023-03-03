355f1519c227b9306f780d438706216883762104
docentYT <63965954+docentYT@users.noreply.github.com>
2023-03-03 14:55:50 +0100

    Update __init__.py

--- src/osm_easy_api/__init__.py
+++ src/osm_easy_api/__init__.py
@@ -2,15 +2,3 @@ from .data_classes import Node, Way, Relation, OsmChange, Action, Tags
 from .diff import Diff, Frequency
 
 from .api import Api
-
-__all__ = [
-    "Node",
-    "Way",
-    "Relation",
-    "OsmChange",
-    "Action",
-    "Tags",
-    "Diff",
-    "Frequency",
-    "Api"
-]
