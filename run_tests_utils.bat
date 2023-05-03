7bf2af7e8032da6adc90a22b4ef9ccb5412f98ca
docentYT <63965954+docentYT@users.noreply.github.com>
2023-05-03 20:20:33 +0200

    delete disused batch files

--- run_tests_utils.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-:: utils
-echo ### test_join_url ###
-python -m unittest tests/utils/test_join_url.py
-
-echo ### test_write_gzip_to_file ###
-python -m unittest tests/utils/test_write_gzip_to_file.py
