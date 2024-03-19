659866200955760e330c20098a7846b1e99d9058
docentYT <63965954+docentYT@users.noreply.github.com>
2024-03-19 13:52:39 +0100

    v3.0.0

--- src/osm_easy_api/__init__.py
+++ src/osm_easy_api/__init__.py
@@ -1,8 +1,6 @@
 """Python package for parsing osm diffs and communicating with the OpenStreetMap api."""
-VERSION = "2.2.0"
+VERSION = "3.0.0"
 
-# from .data_classes import *
 from . import data_classes
 from . import diff
-
 from . import api
